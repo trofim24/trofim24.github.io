bc1b92b0ea2537ce5207be4fe9a7929d5e0eed20
Alexander Trofimov <alexander.trofimov@onlyoffice.com>
2025-02-06 09:25:53 +0300

    add bug build

---
+++ fr/assets/js/acecf23e.e32c140d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/fr/blog","blogTitle":"Blog","authorsListPath":"/fr/blog/authors"}')}}]);
